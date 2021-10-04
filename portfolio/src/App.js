978f90c1288b30928b72ffd84e782713e3136d8e
CalDevC <mr.alexander5132@gmail.com>
2021-10-04 13:58:03 -0700

    react boiler plate code

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Chase Alexander</h3>
+        <p>
+          Portfolio
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
